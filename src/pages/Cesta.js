23871c6fbab9437c627c935aa7a960bdc33a1e53
lucasmoraist <luksmnt1101@gmail.com>
2023-04-11 09:17:44 -0300

    Criando uma página atrvés de um classe no react-native

---
+++ src/pages/Cesta.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class MeuComponente extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Olá mundo!</Text>
+      </View>
+    );
+  }
+}
+
+export default MeuComponente;
