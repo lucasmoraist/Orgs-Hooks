59b4de8a1c5480b206b81b9e6a57023aa9fbbaa9
lucasmoraist <luksmnt1101@gmail.com>
2023-05-11 11:02:02 -0300

    Topo da tela criado

--- orgsHooks/src/Screens/Home/components/Topo.js
+++ orgsHooks/src/Screens/Home/components/Topo.js
@@ -1,10 +1,36 @@
 import React from "react";
-import { Text, View } from 'react-native'
+import { Text, View, Image, StyleSheet } from 'react-native';
+
+
+import Logo from '../../../assets/logo.png';
 
 export default function Topo(){
-    return <>
-        <View>
-            
+    return (
+        <View style={estilos.header}>
+            <Image style={estilos.img} source={Logo}/>
+            <Text style={estilos.h1}>Olá Lucas</Text>
+            <Text style={estilos.desc}>Encontre os melhores produtores</Text>
         </View>
-    </>
+    )
 }
+
+const estilos = StyleSheet.create({
+    header:{
+        backgroundColor: "#F6F6F6",
+        padding: 16,
+    },
+    img:{
+        width: 70,
+        height: 28
+    },
+    h1:{
+        marginTop: 24,
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight:"bold"
+    },
+    desc:{
+        fontSize: 16,
+        lineHeight: 26
+    }
+})
