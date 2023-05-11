15ec1aba5d819ef54a80934fdea697ee864fce6e
lucasmoraist <luksmnt1101@gmail.com>
2023-05-11 11:01:42 -0300

    Exportando dados que serão carregados

---
+++ orgsHooks/src/service/CarregaDados.js
@@ -0,0 +1,10 @@
+import produtores from '../mocks/ProdutoresMocks'
+import topo from '../mocks/TopoMocks'
+
+export const carregaProdutores = ()=>{
+    return produtores;
+}
+
+export const carregaTopo = ()=>{
+    return topo;
+}
