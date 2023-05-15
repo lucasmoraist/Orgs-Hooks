c3f999eaaadb70867d98c1169455cb38b3b633ec
lucasmoraist <luksmnt1101@gmail.com>
2023-05-15 09:53:00 -0300

    Refatorando o componente Estrelas para que fique mais legivel

--- orgsHooks/src/components/Estrelas.js
+++ orgsHooks/src/components/Estrelas.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { Image, StyleSheet, TouchableOpacity, View } from 'react-native';
-
-import estrela from '../assets/estrela.png';
-import estrelaCinza from '../assets/estrelaCinza.png';
+import Estrela from './Estrela';
 
 export default function Estrelas({
     quantidade: quantidadeAntiga,
@@ -10,27 +8,18 @@ export default function Estrelas({
     grande = false
 }){
     const [quantidade, setQuantidade] = useState(quantidadeAntiga);
-    const estilos = estilosFuncao(grande);
-
-    const getImage = (index) =>{
-        if (index < quantidade){
-            return estrela;
-        }
-        return estrelaCinza;
-    }
 
     const RenderEstrelas = ()=>{
         const listaEstrelas = [];
 
         for(let i = 0; i < 5; i++){
-            listaEstrelas.push(
-                <TouchableOpacity
-                    key={i}
-                    onPress={() => setQuantidade(i+1)}
-                    disabled={!editavel}>
-                    <Image source={getImage(i)} style={estilos.estrela}/>
-                </TouchableOpacity>
-            );
+            listaEstrelas.push(<Estrela
+                key={i} 
+                onPress={() => setQuantidade(i+1)}
+                desabilitada={!editavel}
+                preenchida={i < quantidade}
+                grande={grande}
+            />);
         }
         return listaEstrelas;
     }
@@ -41,13 +30,8 @@ export default function Estrelas({
     )
 }
 
-const estilosFuncao = (grande) => StyleSheet.create({
+const estilos = StyleSheet.create({
     estrelas:{
         flexDirection: 'row'
-    },  
-    estrela:{
-        width: grande ? 36 : 12,
-        height: grande ? 36 : 12,
-        marginRight: 2
     }
 })
