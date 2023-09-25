af3c96158ca70b97fac8ce7bda7ea5c85188614f
lucasmoraist <luksmnt1101@gmail.com>
2023-09-25 10:18:37 -0300

    Instalação e configuração do react navigation conforme a documentação

--- orgsHooks/App.js
+++ orgsHooks/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import Home from './src/Screens/Home';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App(){
-  return<>
+export default function App() {
+  return <>
     <SafeAreaView style={estilos.tela}>
-      <Home/>
+      <NavigationContainer>
+        <Home />
+      </NavigationContainer>
     </SafeAreaView>
   </>
 }
 
 const estilos = StyleSheet.create({
-  tela:{
+  tela: {
     flex: 1
   }
 })
