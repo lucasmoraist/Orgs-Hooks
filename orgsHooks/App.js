0ddb7b1c617dcb9e38fbe3f676e5ce5d0e7407dc
lucasmoraist <luksmnt1101@gmail.com>
2023-09-26 14:32:26 -0300

    Navegação em bottom tab

--- orgsHooks/App.js
+++ orgsHooks/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import { StatusBar, SafeAreaView } from 'react-native';
-import useProdutores from './src/hooks/useProdutores';
 
-import Cesta from './src/screen/Cesta';
-import Produtores from './src/screen/Home';
+import AppRotas from './src/routes/AppRotas';
 
 export default function App() {
-  const produtores = useProdutores(false);
-
   return <SafeAreaView style={{ flex: 1 }}>
     <StatusBar />
-    <Produtores melhoresProdutores={false} />
-    {/* {produtores.length > 0 && 
-      <Cesta produtor={{
-        nome: produtores[0].nome, 
-        imagem: produtores[0].imagem
-      }}
-      {...produtores[0].cestas[0]} />
-    } */}
+    <AppRotas/>
   </SafeAreaView>
 }
---
+++ orgsHooks/src/screen/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Produtores from "./Produtores";
+
+const Home = () => {
+    return <Produtores melhoresProdutores={false}/>
+}
+
+export default Home;
